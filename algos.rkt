f9d67797fb87f73deef91476a9bd421522ace0af
Sourav Datta <soura.jagat@gmail.com>
2023-12-26 19:31:01 +0530

    A max arg for stopping functions with no fixed points

--- algos.rkt
+++ algos.rkt
@@ -17,15 +17,20 @@
        [x exp (f x)])
     ((>= i n) x)))
 
-(define (fixed-point-list f exp #:test [test equal?])
+(define (fixed-point-list f exp #:test [test equal?] #:max [max 0])
   (do ([x exp (f x)]
+       [i 0 (+ i 1)]
        [a '() (cons x a)])
-    ((and (> (length a) 1)
-          (test (first a)
-                (second a)))
-     a)))
+    ((or (and (> max 0)
+              (> i max))
+         (and (> (length a) 1)
+              (test (first a)
+                    (second a))))
+     (if (and (> max 0) (> i max))
+         #f
+         a))))
 
-(define (fixed-point f exp #:test [test equal?])
+(define (fixed-point f exp #:test [test equal?] #:max [max 0])
   (let ([ls (fixed-point-list f exp #:test test)])
     (if (empty? ls)
         #f
